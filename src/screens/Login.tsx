c102daef3eac3d7123031961236c487706d9dd66
Judge <judgegodwins@gmail.com>
2021-09-09 15:30:45 +0100

    remove style-component import

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,7 +1,6 @@
 import Padding from '../components/Padding';
 import TextField from '../components/TextField';
 import React, { Component } from 'react';
-import styled from 'styled-components';
 import AuthEnrollCard from '../components/FormComponents/AuthEnrollCard';
 import Button from '../components/Button';
 import Typography from '../components/Typography';
